44e3500f24badb1e911b1e77bf4cee66e5a72c1d
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-01 11:13:01 +0100

    seeding with paperclip

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Card.destroy_all
 
-# colors = %w(#F2F2F0 #11BFAE #456173 #1B3C59)
+colors = %w(#F2F2F0 #11BFAE #456173 #1B3C59)
 
 # 1000.times do 
 #   Card.create(color: colors.sample)
 # end
 
 for i in 1..12
-  file = File.open(File.join(Rails.root,"app/assets/images/#{i}.jpg"))
-  Card.create()
+  picture = File.open(File.join(Rails.root,"app/assets/images/#{i}.jpg"))
+  Card.create(color: colors.sample, picture: picture)
 end
