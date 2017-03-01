2343422ea48ed4bdd5ae2f30a9723fc442015c33
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-01 11:09:50 +0100

    add picture to cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :users, optional: true
+
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
