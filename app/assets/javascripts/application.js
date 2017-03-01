3974083bd1b4981e11a43c56b3196146f1323e51
Julien Da Silva <julien.da.silva@hotmail.com>
2017-03-01 12:20:41 +0100

    playing with js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,9 @@
 
 $( document ).ready(function() {
   $("#sign_up").modal({show:false });
+
+  $(".card").click(function(){
+    $(this).css('background-image','none');
+    $(this).css('background-color','#F2F2F0');
+  });
 });
